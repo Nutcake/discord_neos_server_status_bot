914d7ced3acecd5f6935e9b58003b3167685bc39
Nutcake <Nusskuchen95@hotmail.de>
2022-02-17 08:43:21 +0100

    Add basic HTTP-Response and proper socket closing

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import configparser
 import logging
 
+import discord.errors
+
 from neos_player_count_client import NeosPlayerCountClient
 
 
@@ -25,8 +27,10 @@ def main():
     async def on_ready():
         await bot.create_listener_socket()
         logging.info("Bot connected")
-
-    bot.run(token)
+    try:
+        bot.run(token)
+    except discord.errors.LoginFailure:
+        logging.critical("Specified token is invalid.")
 
 
 if __name__ == '__main__':
--- src/player_count_protocol.py
+++ src/player_count_protocol.py
@@ -1,19 +1,26 @@
 import asyncio
 import logging
-from asyncio import Protocol
+from asyncio import Protocol, transports
 
 import discord
 
 
 class PlayerCountProtocol(Protocol):
+    response_ok = "200 OK"
+    response_br = "400 Bad Request"
+
     def __init__(self, client: discord.Client, loop: asyncio.AbstractEventLoop):
         super().__init__()
         self._client = client
         self._loop = loop
         self._last_count = 0
+        self._transport = None
+
+    def connection_made(self, transport: transports.BaseTransport) -> None:
+        self._transport = transport
 
     def eof_received(self):
-        pass
+        self._transport = None
 
     def data_received(self, data):
         message = data.decode().rsplit("\n")[-1]
@@ -21,15 +28,18 @@ class PlayerCountProtocol(Protocol):
             count = int(message)
         except ValueError:
             logging.warning(f"Cannot parse received message\n'{message}'\nas an integer, ignoring...")
+            self.respond(self.response_br)
             return
 
         # sanity check:
         if count < 0:
             logging.warning(f"Received datagram appears to contain an invalid playercount '{count}', ignoring...")
+            self.respond(self.response_br)
             return
 
         if count == self._last_count:
             logging.info("Count unchanged, skipping...")
+            self.respond(self.response_ok)
             return
 
         logging.info(f"Got playercount {count}, updating presence...")
@@ -39,3 +49,11 @@ class PlayerCountProtocol(Protocol):
                 name=f"with {count} Users",
                 type=discord.ActivityType.playing
             )))
+
+        self.respond(self.response_ok)
+
+    def respond(self, response: str):
+        if self._transport is None:
+            return
+        self._transport.write(f"HTTP/1.1 {response}\n\n".encode('ascii'))
+        self._transport.close()
