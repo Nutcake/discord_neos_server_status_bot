f535cfa32d85cf8a5b98c804683e6ddc097b4b59
Nutcake <Nusskuchen95@hotmail.de>
2022-02-17 07:30:01 +0100

    Switch to TCP-Server and use POST-Messages as supported by NEOS for playercount

--- src/player_count_listener.py
+++ src/player_count_listener.py
@@ -8,14 +8,15 @@ from player_count_protocol import PlayerCountProtocol
 
 class PlayerCountListener:
     def __init__(self, loop: asyncio.AbstractEventLoop, host: str, port: int):
-        family, _, _, _, addr = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_DGRAM)[0]
-        self._addr = addr
+        self._host = host
+        self._port = port
         self._loop = loop
-        self._transport = None
+        self._server = None
 
     async def create_socket(self, bot: discord.Client) -> None:
-        self._transport, _ = await self._loop.create_datagram_endpoint(
+        self._server = await self._loop.create_server(
             lambda: PlayerCountProtocol(bot, self._loop),
             family=socket.AF_INET,
-            local_addr=self._addr
+            port=self._port,
+            start_serving=True
         )
